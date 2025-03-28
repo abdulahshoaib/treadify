6191e4aaaff5a9d035f98ca8a2cf213b0f4591c5
WhoisCipher <khan.abdullah.shoaib@gmail.com>
2025-03-29 01:13:57 +0500

    feat: import all routes as a package

---
+++ backend/src/routes/routes.ts
@@ -0,0 +1,15 @@
+import authRoutes from "./authRoutes.ts";
+import userRoutes from "./profileRoutes.ts";
+import productRoutes from "./productRoutes.ts";
+import featureRoutes from "./featureRoutes.ts";
+import progressRoutes from "./progressRoutes.ts";
+import messageRoutes from "./messagesRoutes.ts";
+
+export default {
+    authRoutes,
+    userRoutes,
+    productRoutes,
+    featureRoutes,
+    progressRoutes,
+    messageRoutes
+}
