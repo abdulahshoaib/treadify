4032aab3fee93903f467980a6c09662dfcff241d
WhoisCipher <khan.abdullah.shoaib@gmail.com>
2025-03-29 21:25:11 +0500

    feat: Add github to the import package

--- backend/src/handlers/auth/authpackage.ts
+++ backend/src/handlers/auth/authpackage.ts
@@ -1,9 +1,11 @@
 import { login } from "./login.ts"
 import { signup } from "./signup.ts"
 import { logout } from "./logout.ts"
+import github from "./github.ts"
 
 export const Auth = {
     login,
     signup,
-    logout
+    logout,
+    github
 }
