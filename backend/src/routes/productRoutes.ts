b83f5e02f478407ddf1b75451347b08478efb3a1
WhoisCipher <khan.abdullah.shoaib@gmail.com>
2025-03-30 11:43:07 +0500

    fix: Update routes for the new routing system

--- backend/src/routes/productRoutes.ts
+++ backend/src/routes/productRoutes.ts
@@ -4,15 +4,14 @@ import Product from "../handlers/product/product.ts";
 const router = Router();
 
 router.post("/", Product.createProductChannel);
-router.post("/:channelID/invite", Product.inviteToChannel);
-router.post("/:channelID/features", Product.addFeature);
-router.patch("/:channelID/deprecate", Product.deprecateChannel);
-router.patch("/:channelID/features/:featurechannel/tl", Product.updateFeatureTL);
-router.patch("/:channelID/features/:featurechannel/deadline", Product.updateFeatureDeadline);
-router.get("/:channelID/deadline", Product.getChannelDeadline);
-router.get("/:channelID/members", Product.getChannelMembers);
-router.get("/:channelID/goals", Product.getChannelGoals);
-router.get("/:channelID/report", Product.getChannelReport);
+router.post("/invite", Product.inviteToChannel);
+router.post("/features", Product.addFeature);
+router.patch("/deprecate", Product.deprecateChannel);
+router.patch("/features/deadline", Product.updateFeatureDeadline);
+router.get("/deadline", Product.getChannelDeadline);
+router.get("/members", Product.getChannelMembers);
+router.get("/goals", Product.getChannelGoals);
+router.get("/report", Product.getChannelReport);
 
 export default router;
 
