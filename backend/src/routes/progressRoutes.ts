88937b4dc3ff10cd8ae210ba2701814b5a2076ba
WhoisCipher <khan.abdullah.shoaib@gmail.com>
2025-04-01 02:22:50 +0500

    feat: Add new route goal/status for getting all goals of a feature

--- backend/src/routes/progressRoutes.ts
+++ backend/src/routes/progressRoutes.ts
@@ -6,6 +6,7 @@ const router = Router();
 router.get("/productchannel/progress", Progress.getProductProgress);
 router.get("/featurechannel/progress", Progress.getFeatureProgress);
 router.get("/feature/active", Progress.getActiveFeature);
+router.get("/goal/status", Progress.getGoalStatus);
 router.get("/featurechannel/commit/:goalid/status", Progress.getCommitStatus);
 
 export default router;
