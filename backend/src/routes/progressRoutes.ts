87460acfff3e2fdec8bff1306f9445dbf10d5fb5
WhoisCipher <khan.abdullah.shoaib@gmail.com>
2025-04-01 02:27:12 +0500

    feat: Add view to get all commit overviews

--- backend/src/routes/progressRoutes.ts
+++ backend/src/routes/progressRoutes.ts
@@ -7,6 +7,7 @@ router.get("/productchannel/progress", Progress.getProductProgress);
 router.get("/featurechannel/progress", Progress.getFeatureProgress);
 router.get("/feature/active", Progress.getActiveFeature);
 router.get("/goal/status", Progress.getGoalStatus);
+router.get("/commit/overview", Progress.getCommitOverview);
 router.get("/featurechannel/commit/:goalid/status", Progress.getCommitStatus);
 
 export default router;
