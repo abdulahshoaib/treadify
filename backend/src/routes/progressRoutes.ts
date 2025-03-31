e509230ca2e9118d986ffcfafa8220feebea86d1
WhoisCipher <khan.abdullah.shoaib@gmail.com>
2025-04-01 02:16:04 +0500

    feat: New route /progress/feature/active for getting features of a productchannel

--- backend/src/routes/progressRoutes.ts
+++ backend/src/routes/progressRoutes.ts
@@ -5,6 +5,7 @@ const router = Router();
 
 router.get("/productchannel/progress", Progress.getProductProgress);
 router.get("/featurechannel/progress", Progress.getFeatureProgress);
+router.get("/feature/active", Progress.getActiveFeature);
 router.get("/featurechannel/commit/:goalid/status", Progress.getCommitStatus);
 
 export default router;
