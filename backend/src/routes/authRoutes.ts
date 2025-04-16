226f1bc9aae07c543a9ae56cdfbad2126a6e8ab5
WhoisCipher <khan.abdullah.shoaib@gmail.com>
2025-04-16 23:58:08 +0500

    feat: Add route for the checking for the username

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -3,6 +3,7 @@ import { Auth } from "../handlers/auth/authpackage.ts"
 
 const authRouter = Router();
 
+authRouter.get("/checkUsername/:username", Auth.checkUsername);
 authRouter.post("/signup", Auth.signup);
 authRouter.post("/login", Auth.login);
 authRouter.post("/logout", Auth.logout);
