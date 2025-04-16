9c47089217b3b872f8480e225f8d9420f61ca633
WhoisCipher <khan.abdullah.shoaib@gmail.com>
2025-04-17 00:00:44 +0500

    feat: Add the checkUsername function to the package

--- backend/src/handlers/auth/authpackage.ts
+++ backend/src/handlers/auth/authpackage.ts
@@ -1,9 +1,10 @@
 import { login } from "./login.ts"
-import { signup } from "./signup.ts"
+import { signup, checkUsername } from "./signup.ts"
 import { logout } from "./logout.ts"
 import github from "./github.ts"
 
 export const Auth = {
+    checkUsername,
     login,
     signup,
     logout,
