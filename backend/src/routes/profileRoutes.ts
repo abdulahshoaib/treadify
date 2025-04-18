ffe9daef1127571185e8f6f0a9354ce57162428d
WhoisCipher <khan.abdullah.shoaib@gmail.com>
2025-04-19 00:20:00 +0500

    misc

--- backend/src/routes/profileRoutes.ts
+++ backend/src/routes/profileRoutes.ts
@@ -3,8 +3,8 @@ import Profile from "../handlers/profile/profile.ts";
 
 const router = Router();
 
-router.get("/:username/profile", Profile.getUserProfile);
-router.patch("/:username/profile", Profile.updateUserProfile);
+router.get("/:username/profile", Profile.getUserProfile as any);
+router.patch("/:username/profile", Profile.updateUserProfile as any);
 
 export default router;
 
