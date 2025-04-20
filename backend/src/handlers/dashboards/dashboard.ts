f67c1fac3d34d259b653b52673c603183d2c64ed
WhoisCipher <khan.abdullah.shoaib@gmail.com>
2025-04-20 12:31:50 +0500

    feat: remove the debuging

--- backend/src/handlers/dashboards/dashboard.ts
+++ backend/src/handlers/dashboards/dashboard.ts
@@ -1,28 +1,31 @@
 import type { Request, Response } from "express"
 import { query } from "../../database/query.ts"
 
-export const dashboard = async (req: Request, res: Response) => {
-    if (!req.session){
-        console.log("Unauthorized")
+export const getDashboard = async (req: Request, res: Response) => {
+    if (!req.session.User)
         return res.status(401).json({ error: "Unauthorized" })
-    }
 
     try {
         const username = req.session.User?.username
 
         const userRes = await query(
-            `SELECT u.*            FROM Users u
-
+            `SELECT u.*, ug.GitHubUsername, r.Name
+            FROM Users u
+            LEFT JOIN UserRoles ur ON u.UserID = ur.UserID
+            LEFT JOIN Roles r ON ur.RoleID = r.RoleID
+            JOIN UserGitHubIntegration ug ON u.UserID = ug.UserID
             WHERE u.Username = @username`,
             { username }
         )
 
-        if (userRes.length === 0){
-            console.log("No user found")
+        if (userRes.length === 0) {
             return res.status(401).json({ error: "No User Found" })
         }
 
-        return res.status(200).json({ role: userRes[0].Name, name: userRes[0].FirstName + " " + userRes[0].LastName })
+        res.status(200).json({
+            success: true,
+            role: userRes[0].Name,
+        });
 
     } catch (err: any) {
         console.error(err.message)
