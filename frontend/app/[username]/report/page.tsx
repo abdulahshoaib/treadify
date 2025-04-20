9bb65fc1c4af6aba3815124aab28f6ffce4b5ba0
WhoisCipher <khan.abdullah.shoaib@gmail.com>
2025-04-20 12:49:40 +0500

    feat: Report page to show progress to TL and PM

---
+++ frontend/app/[username]/report/page.tsx
@@ -0,0 +1,12 @@
+export default function ReportPage() {
+    return (
+        <main className="relative z-10 flex-1 p-6">
+            <div className="mb-8">
+                <h1 className="text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-cyan-400 to-blue-400">
+                    Reports
+                </h1>
+            </div>
+        </main>
+    )
+}
+
