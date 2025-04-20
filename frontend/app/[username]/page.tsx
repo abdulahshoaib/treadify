3ac23bb6811d7a11b03677cec15c72442942c67d
WhoisCipher <khan.abdullah.shoaib@gmail.com>
2025-04-20 12:43:19 +0500

    feat: main dashboard route, showed upon login

--- frontend/app/[username]/page.tsx
+++ frontend/app/[username]/page.tsx
@@ -1,35 +1,71 @@
-import { notFound, redirect } from "next/navigation"
+import { notFound } from "next/navigation"
+import { headers } from "next/headers"
+import { redirect } from "next/navigation"
 
 import DevDashboard from "@/components/DevDashboard"
 import TLDashboard from "@/components/TLDashboard"
 import PMDashboard from "@/components/PMDashboard"
 
-export default async function DashboardPage({ params }: { params: { username: string } }) {
-    const { username } = params
+type Repo = {
+    name: string
+}
 
-    const role = await fetch(`http://localhost:5000/dashboard/${username}`, {
+export default async function DashboardPage() {
+    const headerList = await headers()
+    const cookie = headerList.get("cookie") || ""
+
+    // get userRole to render the dashboard
+    const dashboardRes = await fetch(`http://localhost:5000/dashboard`, {
         method: "GET",
-        headers: { application: "json" },
-        credentials: "include",
-        cache: "no-store",
+        headers: {
+            Accept: "application/json",
+            Cookie: cookie
+        },
     })
 
-    if (role.status === 401)
-        redirect("/login")
+    if (!dashboardRes.ok) {
+        if (dashboardRes.status === 401) {
+            redirect("/login")
+        }
+        throw new Error("Server Error " + dashboardRes.status) as any
+    }
+    console.log("dashboardRes: " + dashboardRes.status)
 
-    const data = await role.json()
+    const data = await dashboardRes.json()
+    if (!dashboardRes.ok) console.log("data: " + data.error)
 
-    if (!data)
-        notFound()
+    let repos: Repo[] = []
+    if (data.role === "Product Manager") {
+        // fetch the userRepos
+        const repoRes = await fetch(`http://localhost:5000/user/repos`, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                Cookie: cookie
+            },
+        })
+        if (!repoRes.ok) {
+            if (repoRes.status === 401) {
+                redirect("/login")
+            }
+            throw new Error("Server Error " + repoRes.status) as any
+        }
 
-    console.log(data.role)
+        const repoData = await repoRes.json()
+        repos = repoData.data?.repositories || []
+
+        if (!repoRes.ok) console.log("data: " + repoData.error)
+        else console.log("repoData: " + repoData.data)
+    }
+
+
     switch (data.role) {
         case "Developer":
-            return <DevDashboard Role={data.role} Name={data.name} />
+            return <DevDashboard />
         case "Technical Lead":
-            return <TLDashboard Role={data.role} Name={data.name} />
+            return <TLDashboard />
         case "Product Manager":
-            return <PMDashboard Role={data.role} Name={data.name} />
+            return <PMDashboard repos={repos} />
         default:
             notFound()
     }
