a3301929f9548a82823aedb97295e43280f1d1f4
WhoisCipher <khan.abdullah.shoaib@gmail.com>
2025-04-20 12:37:37 +0500

    feat: next api route for calling backend route as headers can only be accessed in SSR components

---
+++ frontend/app/api/create-product/route.ts
@@ -0,0 +1,40 @@
+import { headers } from "next/headers"
+import { NextResponse } from "next/server"
+
+export async function POST(req: Request) {
+    console.log("API route hit: create-product");
+    try {
+        const body = await req.json();
+        console.log("Request body:", body);
+        const { Name, RepoName, Deadline } = body;
+
+        const header = await headers();
+        const cookie = header.get("cookie") || "";
+
+
+        console.log("\n\n", Name, RepoName, Deadline)
+        const res = await fetch("http://localhost:5000/productchannel", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Cookie: cookie,
+            },
+            body: JSON.stringify({ Name, RepoName, Deadline }),
+        });
+
+        console.log("Backend response status:", res.status);
+        const data = await res.json();
+        console.log("Backend response data:", data.error);
+
+        if (!res.ok) {
+            console.error("Error from backend:", data);
+            return NextResponse.json({ error: data }, { status: res.status });
+        }
+
+        return NextResponse.json(data);
+    } catch (error) {
+        console.error("Error in API route:", error);
+        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    }
+}
