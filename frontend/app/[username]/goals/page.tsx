d93b2ed559b1c9b909f7f570e000348346c0f689
WhoisCipher <khan.abdullah.shoaib@gmail.com>
2025-04-23 09:39:46 +0500

    feat: server side of the goals page that sends server responses to the client

--- frontend/app/[username]/goals/page.tsx
+++ frontend/app/[username]/goals/page.tsx
@@ -1,11 +1,62 @@
-export default function GoalsPage() {
+import { headers } from "next/headers"
+import { redirect, notFound } from "next/navigation"
+
+import GoalsClient from "./goals-client"
+
+export default async function GoalsPage({ params }: { params: { username: string } }) {
+    const headerList = await headers()
+    const cookie = headerList.get("cookie") || ""
+
+    const goalsRes = await fetch(`http://localhost:5000/featurechannel/goals`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Cookie: cookie
+        }
+    })
+
+    const goal = await goalsRes.json()
+    console.log(goal)
+
+    if (!goalsRes.ok) {
+        if (goalsRes.status === 401) {
+            redirect("/login")
+        }
+        throw new Error("Server Error " + goalsRes.status)
+    }
+
+    const dashboardRes = await fetch(`http://localhost:5000/dashboard`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Cookie: cookie,
+        },
+    })
+
+    if (!dashboardRes.ok) {
+        if (dashboardRes.status === 401) {
+            redirect("/login")
+        }
+        throw new Error("Server Error " + dashboardRes.status)
+    }
+
+    const data = await dashboardRes.json()
+    const loggedInUsername = data.username
+    const role = data.role
+
+    const param = await params
+    console.log(loggedInUsername)
+    if (param.username !== loggedInUsername) {
+        redirect(`/${loggedInUsername}`)
+    }
+
+    if (role !== "Developer" && role !== "Technical Lead") {
+        notFound()
+    }
+
     return (
         <main className="relative z-10 flex-1 p-6">
-            <div className="mb-8">
-                <h1 className="text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-cyan-400 to-blue-400">
-                    Goals
-                </h1>
-            </div>
+            <GoalsClient goalData={goal.data} />
         </main>
     )
 }
