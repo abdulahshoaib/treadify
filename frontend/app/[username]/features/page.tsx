f633138b31df957ca823dfd156361beb87422afd
WhoisCipher <khan.abdullah.shoaib@gmail.com>
2025-04-23 09:38:56 +0500

    feat: server side of the feature page that sends server responses to the client

--- frontend/app/[username]/features/page.tsx
+++ frontend/app/[username]/features/page.tsx
@@ -1,12 +1,82 @@
-export default function FeaturePage() {
+import { headers } from "next/headers"
+import { redirect, notFound } from "next/navigation"
+
+import FeaturesClient from "./features-client"
+
+export default async function GoalsPage({ params }: { params: { username: string } }) {
+    const headerList = await headers()
+    const cookie = headerList.get("cookie") || ""
+
+    // get TLs
+    const TLRes = await fetch(`http://localhost:5000/productchannel/tl`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Cookie: cookie,
+        },
+    })
+    if (!TLRes.ok) {
+        if (TLRes.status === 401) {
+            redirect("/login")
+        }
+        throw new Error("Server Error " + TLRes.status)
+    }
+    const TLData = await TLRes.json()
+    console.log("Team Leads: ", TLData)
+
+    // GET features
+    const featureRes = await fetch(`http://localhost:5000/productchannel/features`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Cookie: cookie,
+        },
+    })
+    if (!featureRes.ok) {
+        if (featureRes.status === 401) {
+            redirect("/login")
+        }
+        throw new Error("Server Error " + featureRes.status)
+    }
+    const featData = await featureRes.json()
+    console.log("Features: ", featData)
+
+
+    const dashboardRes = await fetch(`http://localhost:5000/dashboard`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Cookie: cookie,
+        },
+    })
+
+    if (!dashboardRes.ok) {
+        if (dashboardRes.status === 401) {
+            redirect("/login")
+        }
+        throw new Error("Server Error " + dashboardRes.status)
+    }
+
+    const data = await dashboardRes.json()
+    const loggedInUsername = data.username
+    const role = data.role
+
+    const param = await params
+    if (param.username !== loggedInUsername) {
+        redirect(`/${loggedInUsername}`)
+    }
+
+    if (role !== "Product Manager" && role !== "Technical Lead") {
+        notFound()
+    }
+
+    const featureData = featData.data
 
     return (
-        <main className="relative z-10 flex-1 p-6">
-            <div className="mb-8">
-                <h1 className="text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-cyan-400 to-blue-400">
-                    Features
-                </h1>
-            </div>
+        <main className="relative z-10 flex-1 p-5">
+
+            <FeaturesClient initialFeatures={featureData} />
+
         </main>
     )
 }
